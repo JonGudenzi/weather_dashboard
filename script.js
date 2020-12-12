43778e254b204acacfab3d1636376855247e240a
JonGudenzi <jongudenzi@gmail.com>
2020-12-11 20:49:26 -0500

    created buttons for previous searches that are save in local storage

--- script.js
+++ script.js
@@ -3,43 +3,68 @@ let inputEl = $('input[name="cityInput"]');
 
 $(".submit").on("click", function () {
     let city = $(".inputValue").val();
-
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIkey}`;
-//calling first API to retreive lon/lat data to use in the second API
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        success: function () {
-        }
-    })
-        .then(function (data) {
-            createWeatherBlock(data);
-            let lat = JSON.stringify(data.coord.lat);
-            let lon = JSON.stringify(data.coord.lon);
-            let queryURLSecond = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&lon=${lon}&appid=${APIkey}`;
-            $.ajax({
-                url: queryURLSecond,
-                method: "GET"
-            })
-                .then(function (data2) {
-                    forecastBlock(data2);
-
-                    let obj = data2;
-                    console.log(obj);
-                });
-        })
-//saving searches to local storage
-    var textHistory = JSON.parse(localStorage.getItem("text")) || [];
-    textHistory.push(city);
-    localStorage.setItem("text", JSON.stringify(textHistory));
+    dataRender(city);
 })
+
+function dataRender(city){
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIkey}`;
+    //calling first API to retreive lon/lat data to use in the second API
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            success: function () {
+            }
+        })
+            .then(function (data) {
+                createWeatherBlock(data, city);
+                let lat = JSON.stringify(data.coord.lat);
+                let lon = JSON.stringify(data.coord.lon);
+                let queryURLSecond = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&lon=${lon}&appid=${APIkey}`;
+                $.ajax({
+                    url: queryURLSecond,
+                    method: "GET"
+                })
+                    .then(function (data2) {
+                        forecastBlock(data2);
+    
+                        let obj = data2;
+                        console.log(obj);
+                    });
+            })
+    //saving searches to local storage
+        var textHistory = JSON.parse(localStorage.getItem("text")) || [];
+        textHistory.push(city);
+        localStorage.setItem("text", JSON.stringify(textHistory));
+        renderSaveSearchBtns();
+}
+
+
+$("body").on("click", ".cityBtn", function(){
+  var save = $(this).text();
+    dataRender(save);
+});
+
+function renderSaveSearchBtns() {
+    $(".cityBtnContainer").empty();
+    var currentData = JSON.parse(localStorage.getItem("text")) || [];
+    currentData.forEach(function (searchData) {
+      var btnData = $("<button>").addClass("cityBtn");
+      console.log(btnData);
+      btnData.text(searchData);
+      $(".cityBtnContainer").append(btnData);
+    });
+  }
+
+  renderSaveSearchBtns();
+
+
 //current weather container
-function createWeatherBlock(data){
+function createWeatherBlock(data, city){
     let today = moment();
     $(".currentWeather").text(today.format("MMMM Do, YYYY"));
     $(".ul_city").append("<li>" + inputEl.val() );
     let weatherEl = $("<div>")
-    let cityName = $("<h1>" + inputEl.val() + "</h1>") ;
+    let cityName = $("<h1>" + city + "</h1>") ;
     let tempEl = $("<p>Temp: " + data.main.temp + "</p>");
     let humidityEl = $("<p>Humidity: " + data.main.humidity + "</p>");
     let windEl = $("<p>Wind Speed: " + data.wind.speed + "</p>");
