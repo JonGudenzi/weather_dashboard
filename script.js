278387fd93b09d017a32a5f658fdafcd01d874f5
JonGudenzi <jongudenzi@gmail.com>
2020-12-19 15:27:30 -0500

    used loop for 5 days forcast instead of listing each div for each day. fixed some css to space out containers

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let inputEl = $('input[name="cityInput"]');
 $(".submit").on("click", function () {
     let city = $(".inputValue").val();
     $(".inputValue").val("");
-     //saving searches to local storage
-     var textHistory = JSON.parse(localStorage.getItem("text")) || [];
-     textHistory.push(city);
-     localStorage.setItem("text", JSON.stringify(textHistory));
+    //saving searches to local storage
+    var textHistory = JSON.parse(localStorage.getItem("text")) || [];
+    textHistory.push(city);
+    localStorage.setItem("text", JSON.stringify(textHistory));
     dataRender(city);
 })
 
@@ -36,7 +36,7 @@ function dataRender(city) {
                     console.log(obj);
                 });
         })
-   
+
     renderSaveSearchBtns();
 }
 
@@ -76,62 +76,29 @@ function forecastBlock(forcast) {
     // current
     let UVi = $("<p>UV Index: " + forcast.current.uvi + "</p>")
     $("#currentWeather").append(UVi);
-    //start of 5 day forcast
+
     //day One
     let tomorrow = moment().add(1, "days")
-    $("#day_One").text(tomorrow.format("MMMM Do, YYYY"));
-    let dayOne = $("<div>");
+    let dayOne = $('<div class="days">');
+    dayOne.text(tomorrow.format("MMMM Do, YYYY"));
     let tempForcastEl = $("<p>Temp: " + JSON.stringify(forcast.daily[0].temp.day) + "</p>");
     let iconEl = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[0].weather[0].icon + ".png'>");
     let humidityForcastEl = $("<p>Humidity: " + JSON.stringify(forcast.daily[0].humidity) + "%" + "</p>");
     dayOne.append(tempForcastEl, iconEl, humidityForcastEl);
-    $("#day_One").append(dayOne);
-
-    // day two
-    let second_Day = moment().add(2, "days")
-    $("#day_Two").text(second_Day.format("MMMM Do, YYYY"));
-    let dayTwo = $("<div>");
-    let tempForcastEl2 = $("<p>Temp: " + JSON.stringify(forcast.daily[1].temp.day) + "</p>");
-    let iconEl2 = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[1].weather[0].icon + ".png'>");
-    let humidityForcastEl2 = $("<p>Humidity: " + JSON.stringify(forcast.daily[1].humidity) + "%" + "</p>");
-    dayTwo.append(tempForcastEl2, iconEl2, humidityForcastEl2);
-    $("#day_Two").append(dayTwo);
-
-    // day three
-    let Third_Day = moment().add(3, "days")
-    $("#day_Three").text(Third_Day.format("MMMM Do, YYYY"));
-    let dayThree = $("<div>");
-    let tempForcastEl3 = $("<p>Temp: " + JSON.stringify(forcast.daily[2].temp.day) + "</p>");
-    let iconEl3 = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[2].weather[0].icon + ".png'>");
-    let humidityForcastEl3 = $("<p>Humidity: " + JSON.stringify(forcast.daily[2].humidity) + "%" + "</p>");
-    dayThree.append(tempForcastEl3, iconEl3, humidityForcastEl3);
-    $("#day_Three").append(dayThree);
-
-    // day four
-    let Forth_Day = moment().add(4, "days")
-    $("#day_Four").text(Forth_Day.format("MMMM Do, YYYY"));
-    let dayFour = $("<div>");
-    let tempForcastEl4 = $("<p>Temp: " + JSON.stringify(forcast.daily[3].temp.day) + "</p>");
-    let iconEl4 = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[3].weather[0].icon + ".png'>");
-    let humidityForcastEl4 = $("<p>Humidity: " + JSON.stringify(forcast.daily[3].humidity) + "%" + "</p>");
-    dayFour.append(tempForcastEl4, iconEl4, humidityForcastEl4);
-    $("#day_Four").append(dayFour);
-
-    // day five
-    let Fifth_Day = moment().add(5, "days")
-    $("#day_Five").text(Fifth_Day.format("MMMM Do, YYYY"));
-    let dayFive = $("<div>");
-    let tempForcastEl5 = $("<p>Temp: " + JSON.stringify(forcast.daily[4].temp.day) + "</p>");
-    let iconEl5 = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[4].weather[0].icon + ".png'>");
-    let humidityForcastEl5 = $("<p>Humidity: " + JSON.stringify(forcast.daily[4].humidity) + "%" + "</p>");
-    dayFive.append(tempForcastEl5, iconEl5, humidityForcastEl5);
-    $("#day_Five").append(dayFive);
+    $("#forcastContainer").append(dayOne);
     currentWeather.style.display = "inline-block";
-    day_One.style.display = "inline-block";
-    day_Two.style.display = "inline-block";
-    day_Three.style.display = "inline-block";
-    day_Four.style.display = "inline-block";
-    day_Five.style.display = "inline-block";
+
+    //next 4 days
+    for (i = 1; i <= 4; i++) {
+        let second_Day = moment().add(i + 1, "days")
+        let day = $('<div class="days">');
+        day.text(second_Day.format("MMMM Do, YYYY"));
+        let tempForcastEl = $("<p>Temp: " + JSON.stringify(forcast.daily[i].temp.day) + "</p>");
+        let iconEl = $("<img src='http://openweathermap.org/img/wn/" + forcast.daily[i].weather[0].icon + ".png'>");
+        let humidityForcastEl = $("<p>Humidity: " + JSON.stringify(forcast.daily[i].humidity) + "%" + "</p>");
+        day.append(tempForcastEl, iconEl, humidityForcastEl);
+        $("#forcastContainer").append(day);
+    }
 }
 
 
